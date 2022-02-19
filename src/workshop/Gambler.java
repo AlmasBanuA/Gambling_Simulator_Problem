d62cfa92e0538850d010d539bb4a98f26d400948
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-02-19 11:43:36 +0530

    Gambler would start with a stake of $100 every day and bet $1 every game.

--- src/workshop/Gambler.java
+++ src/workshop/Gambler.java
@@ -1,16 +1,20 @@
 package workshop;
 
-import java.util.Scanner; // import scanner class
 public class Gambler{
 	
-	//UC1 here taking variables 
-	public static final int INITIAL_BET_STAKE = 100;
+	/*
+	* UC1 here taking variables and initializing it in the int data type
+	*/
+	 
+    public static final int INITIAL_BET_STAKE = 100;
     public static final int BET_AMT = 1;
     
     public static void main(String[] args) {
-		System.out.println("welcome to gambler problem");
+	    
+	    /*
+	    * printing welcome message
+	    */
+    System.out.println("welcome to gambler problem");
 		
-		 Scanner scan = new Scanner(System.in); // taking user from input
-		 scan.nextInt();
     }
 }
