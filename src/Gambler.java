54b162474d96ad7c0ab9e3c9d885054e4d508fe9
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-02-19 11:48:16 +0530

    Gambler make $1 bet so either win or loose $1 the game

--- src/Gambler.java
+++ src/Gambler.java
@@ -2,21 +2,27 @@ package workshop;
 
 public class Gambler {
 
-		    //UC1
+		    /*
+	             * UC1 here taking variables and initializing it in the int data type
+	            */
 		    public static final int INITIAL_BET_STAKE = 100;
 		    public static final int BET_AMT = 1;
 
-		    //UC2
+		    /*
+		    * UC2 Calling a method of gambler_SolutionUC2
+		    */
 		    public void gambler_SolutionUC2() {
 
-		        double num = Math.floor(Math.random() * 10) % 2;
+		        double num = Math.floor(Math.random() * 10) % 2;	/*random function using to check either win or loose*/
+			    
 		        int remg_amt = INITIAL_BET_STAKE;
-		        if (num == 0) {
+		        if (num == 0){
 		            remg_amt += BET_AMT;
 		            System.out.println("The Gambler wins the bet and he has $"
 		                    + remg_amt + " with him after one bet.");
 		        } else {
 		            remg_amt -= BET_AMT;
+				
 		            System.out.println("The Gambler loses the bet and he has $"
 		                    + remg_amt + " with him after one bet.");
 
