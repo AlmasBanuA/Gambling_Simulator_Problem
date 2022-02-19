ec761a3422101a2df4a4a82a4b66ae713c445764
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-02-19 08:56:41 +0530

    Gambler problem with initializing varialbes

---
+++ src/workshop/Gambler.java
@@ -0,0 +1,16 @@
+package workshop;
+
+import java.util.Scanner; // import scanner class
+public class Gambler{
+	
+	//UC1 here taking variables 
+	public static final int INITIAL_BET_STAKE = 100;
+    public static final int BET_AMT = 1;
+    
+    public static void main(String[] args) {
+		System.out.println("welcome to gambler problem");
+		
+		 Scanner scan = new Scanner(System.in); // taking user from input
+		 scan.nextInt();
+    }
+}
